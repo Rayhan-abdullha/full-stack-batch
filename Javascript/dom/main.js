d7820d21db83939fc6a2a7476c9b811938b4252f
Rayhan-abdullha <captainrayhan215@gmail.com>
2025-08-02 20:37:51 +0600

    practice

---
+++ Javascript/dom/main.js
@@ -0,0 +1,13 @@
+// const boxWrapper = document.getElementById('box-wrapper');
+// const boxes = boxWrapper.children; // HTMLCollection
+// console.log(boxes)
+// console.log(boxes[0]); // Using Index WORKS!
+// console.log(boxes.namedItem("id-box1")); // Using ID WORKS!
+// console.log(boxes.namedItem("nm-box1")); // Using Name WORKS!
+// console.log(boxes.namedItem("cls-box1")); // Using Class NULL
+
+
+// const title = document.getElementById('heading')
+// console.log(title)
+// title.style.color = 'red'
+// title.setAttribute('title', 'this is title')
