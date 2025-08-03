baf56a149dd783c55ca6493cd64d661b0a399be0
Rayhan-abdullha <captainrayhan215@gmail.com>
2025-08-03 09:12:49 +0600

    todo

---
+++ Javascript/event/main.js
@@ -0,0 +1,19 @@
+// const btn = document.querySelector('button')
+// btn.onclick = function () {
+//     document.body.style.backgroundColor ='gray'
+// }
+// document.getElementById('white').addEventListener('click', function () {
+//     document.body.style.backgroundColor ='white'
+//     console.log('white')
+// })
+
+// add todo
+document.getElementById('todo-form').addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const inputValue = document.getElementById('todo-input').value
+    const ul = document.getElementById('ul')
+    const li = document.createElement('li')
+    li.innerText = inputValue
+    ul.appendChild(li)
+});
