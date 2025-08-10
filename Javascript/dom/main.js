024c0cc2236e8f3cf2525c3b1085acc57a70aac8
Rayhan-abdullha <captainrayhan215@gmail.com>
2025-08-10 16:27:33 +0600

    dom

--- Javascript/dom/main.js
+++ Javascript/dom/main.js
@@ -1,4 +1,4 @@
-const boxWrapper = document.getElementById('box-wrapper');
+// const boxWrapper = document.getElementById('box-wrapper');
 // const boxes = boxWrapper.children; // HTMLCollection
 // console.log(boxes)
 // console.log(boxes[0]); // Using Index WORKS!
@@ -18,3 +18,32 @@ const boxWrapper = document.getElementById('box-wrapper');
 // div.classList.add('demo')
 // div.innerText = 'test'
 // boxWrapper.appendChild(div)
+
+// console.log(window)
+// window.alert("Enter Password")
+// const pass = window.prompt("Enter Your Password")
+// if (parseInt(pass) === 1234) {
+//     console.log('Accessed')
+// }
+
+
+// window.addEventListener('resize', (event) => {
+//     console.log(event.target.outerWidth)
+//     if (event.target.outerWidth > 1000) {
+//         document.body.style.backgroundColor = 'gray'
+//     } else {
+//         document.body.style.backgroundColor = 'white'
+//     }
+// })
+
+
+// create tag
+const element = document.createElement('h1')
+element.innerText = 'Hello JS'
+function style(prop, val) {
+    element.style[`${prop}`] = val
+}
+style('color', 'white')
+style('backgroundColor', 'red')
+document.body.appendChild(element)
+console.log(document.body)
---
+++ Javascript/js.js
@@ -0,0 +1,21 @@
+
+// const arr = [1,2,3]
+// function add(n, array = []) {
+//     array.push(n)
+//     console.log(array)
+// }
+// add(4, arr)
+// add(5, arr)
+// console.log(arr)
+
+
+// function CreateUser (name, age){
+//     this.name = name
+//     this.age = age
+//     this.print = () => {
+//         console.log(`${this.name}, ${this.age}`)
+//     }
+// }
+
+// const user = new CreateUser('coder', 21)
+// user.print()
